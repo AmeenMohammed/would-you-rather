85487a55a5f3c69936eb55d8f138cd840a24c244
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-03-11 23:38:28 +0200

    Format question with all details

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,15 +1,33 @@
-export function formatQuestion (question, author) {
-    const { id, optionOne, optionTwo } = question
-    const { name, avatarURL } = author
-  
-    return {
-      name,
-      id,
-      optionOneText: optionOne.text,
-      optionTwoText: optionTwo.text,
-      avatar: avatarURL,
-    }
+export function formatQuestion (question, author, authedUser) {
+  const { id, optionOne, optionTwo } = question
+  const { name, avatarURL } = author
+  const selectedOptionOne = optionOne.votes.includes(authedUser)
+  const selectedOptionTwo = optionTwo.votes.includes(authedUser)
+  const optionOneVotes = optionOne.votes.length
+  const optionTwoVotes = optionTwo.votes.length
+  const totalVotes = optionOneVotes + optionTwoVotes
+
+  return {
+    name,
+    id,
+    optionOneText: optionOne.text,
+    optionTwoText: optionTwo.text,
+    optionOneVotes: optionOneVotes,
+    optionTwoVotes: optionTwoVotes,
+    totalVotes: totalVotes,
+    percentOptionOne:  percentage(optionOneVotes, totalVotes),
+    percentOptionTwo:  percentage(optionTwoVotes, totalVotes),
+    selectedOptionOne: selectedOptionOne,
+    selectedOptionTwo: selectedOptionTwo,
+    answeredQuestion: selectedOptionOne || selectedOptionTwo,
+    avatar: avatarURL,
   }
+}
+
+function percentage(number, total) {
+  const percent = total !== 0 ? Math.round(number * 10000 / total) / 100 : 0
+  return percent
+}
   export function formatScores (user, questions) {
     const { id, name, avatarURL } = user
     let answered = 0
