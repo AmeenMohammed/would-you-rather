75be164e079fe9ae86b967a680067fd8947420e5
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-03-11 23:04:53 +0200

    Add options functionality

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,26 +2,61 @@ import React, { Component } from 'react'
 
 class NewQuestion extends Component {
 
+  state = {
+   optionOne: '',
+   optionTwo: '',
+  }
+  
+  handleOptionOneChange = (e) => {
+    const optionOneValue = e.target.value
+    this.setState(() => ({
+     optionOne: optionOneValue
+    }))
+  }
+  handleOptionTwoChange = (e) => {
+    const optionTwoValue = e.target.value
+    this.setState(() => ({
+     optionTwo: optionTwoValue
+    }))
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { optionOne, optionTwo } = this.state
+
+
+    //add to store
+    
+    this.setState(() => ({
+      optionOne: '',
+      optionTwo: '',
+    }))
+  }
   render() {
+    const { optionOne, optionTwo } = this.state
 
 
     return (
       <div>
         <h3 className='center'>Create New Question</h3>
-        <form className='new-question'>
+        <form className='new-question' onSubmit={this.handleSubmit}>
           <input
-                      className='question-option'
-
+            className='question-option'
             placeholder="First Option"
+            value={optionOne}
+            onChange={this.handleOptionOneChange}
           />
           <h4 className='center'>OR</h4>
           <input
             className='question-option'
             placeholder="Second Option"
+            value={optionTwo}
+            onChange={this.handleOptionTwoChange}
           />
           <button
             className='button'
-            type='submit'>
+            type='submit'
+            disabled={optionTwo === '' || optionOne === ''}>
               Submit
           </button>
         </form>
