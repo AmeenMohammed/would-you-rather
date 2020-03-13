fce35536ba8d0f0712354253d7e27de647e8b265
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-03-13 16:00:54 +0200

    Add save answer action and reducer

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,6 +1,7 @@
-import { saveQuestion } from '../utils/api'
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ADD_QUESTION = 'ADD_QUESTION'
+export const SAVE_ANSWER = 'SAVE_ANSWER'
 
 
 export function receiveQuestions( questions ){
@@ -9,12 +10,14 @@ export function receiveQuestions( questions ){
       questions,
     }
   }
+
   function addQuestion(question) {
     return {
       type: ADD_QUESTION,
       question,
     }
   }
+
   export function handleAddQuestion(optionOneText, optionTwoText) {
     return (dispatch, getState) => {
       const { authedUser } = getState()
@@ -24,6 +27,27 @@ export function receiveQuestions( questions ){
         author: authedUser,
       })
       .then((question) => dispatch(addQuestion(question)))
+    }
+  }
+
+  function addAnswer(id, answer, authedUser) {
+    return {
+      type: SAVE_ANSWER,
+      id,
+      answer,
+      authedUser
+    }
+  }
+  
+  export function handleSaveAnswer(id, answer) {
+    return (dispatch, getState) => {
+      const { authedUser } = getState()
+      return saveQuestionAnswer({
+        authedUser: authedUser,
+        id: id,
+        answer: answer,
+      })
+      .then(() => dispatch(addAnswer(id, answer, authedUser)))
     }
   }
   
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,6 +1,6 @@
 import { RECEIVE_QUESTIONS } from '../actions/questions'
 import { ADD_QUESTION } from '../actions/questions'
-
+import {SAVE_ANSWER} from '../actions/questions'
 export default function questions (state = {}, action) {
   switch (action.type) {
     case RECEIVE_QUESTIONS:
@@ -12,6 +12,17 @@ export default function questions (state = {}, action) {
         return {
           ...state,
           [action.question.id]: action.question,
+      }
+      case SAVE_ANSWER:
+        return {
+          ...state,
+          [action.id]: {
+            ...state[action.id],
+            [action.answer]: {
+            ...state[action.id][action.answer],
+            votes: state[action.id][action.answer].votes.concat([action.authedUser])
+          }
+        }
       }
     default :
       return state
